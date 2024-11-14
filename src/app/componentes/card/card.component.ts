2c119d0440c4743db17914f4f885fce299ec0d8f
weltonSantana <weltonsantanaa12@gmail.com>
2024-11-14 09:28:12 -0300

    refac: alterando componente de card para usar o button component

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { Pensamento } from './cardModel';
+import { ButtonComponent } from 'src/app/componentes/button/button.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  standalone: true
-})
-export class CardComponent  {
+  standalone: true,
+  imports: [CommonModule, ButtonComponent]
 
+})
+export class CardComponent {
   @Input() pensamento: Pensamento = {
     titulo: "sem titulo",
     descricao: "Vazia"
+  };
+
+  @Input() mode: 'home' | 'produtos' = 'home'; // Define o modo do card
+
+  editarPensamento() {
+    console.log('Editar pensamento:', this.pensamento);
   }
 
+  excluirPensamento() {
+    console.log('Excluir pensamento:', this.pensamento);
+  }
+
+  verDetalhes() {
+    console.log('Ver detalhes do pensamento:', this.pensamento);
+  }
 }
