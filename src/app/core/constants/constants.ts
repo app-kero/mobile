6692241797f489dd0fc37cb12057b37cc061dc20
luccasocastro <emaildocastro.adm01@gmail.com>
2024-11-28 11:56:49 -0300

    feat: listagem de produtos

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -7,6 +7,12 @@ export const ApiEndpoint = {
         Recover: `${apiUrl}/api/recovery/recover-password`,
         Reset: `${apiUrl}/api/recovery/reset-password`
     },
+    Produtos: {
+      All: `${apiUrl}/produtos`,
+      Cadastrar: `${apiUrl}/produtos/new`,
+      BuscarPorTag: `${apiUrl}/produtos/tag`,
+      BuscarPorNome: `${apiUrl}/produtos/`
+    }
 };
 
 export const LocalStorage = {
--- src/app/core/model/common.model.ts
+++ src/app/core/model/common.model.ts
@@ -26,3 +26,27 @@ export interface ApiResponse<T> {
     accessToken?: string;
     data: T;
 }
+
+export interface Imagem {
+    id: number,
+    name: string,
+    contentType: string,
+    size: number,
+    url: string
+}
+
+export interface Tag {
+    id: number,
+    nome: string
+}
+
+export interface Produto {
+    id: number;
+    nome: string;
+    descricao: string;
+    horario: string;
+    local: string;
+    usuario: string;
+    tags: Tag[];
+    fotos: Imagem[];
+}
